05ca43d348f0653852a0090b0756cb6a30f29015
adinugroh0 <aditwist1@gmail.com>
2025-01-14 04:29:48 +0700

    nama

--- components/Beranda.tsx
+++ components/Beranda.tsx
@@ -64,8 +64,8 @@ const Dashboard = () => {
               <table className="w-full bg-gray-800 rounded-lg shadow-lg overflow-hidden">
                 <thead>
                   <tr className="bg-gray-700 text-left text-gray-300">
-                    <th className="p-3">Nama Barang</th>
-                    <th className="p-3">Lokasi</th>
+                    <th className="p-3">Nomor Pelamggam</th>
+                    <th className="p-3">Alamat</th>
                     <th className="p-3">Tanggal</th>
                     <th className="p-3">Periode (Bulan)</th>
                   </tr>
